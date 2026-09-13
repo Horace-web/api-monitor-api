6a5f93cdae6fb15e5bba57bb39468d6c7dfb08aa
Horace-web <horaceodounlami2006@gmail.com>
2026-09-13 04:10:32 +0100

    refactor(common): export target URL security service

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '../app.controller';
+import { TargetUrlService } from './target-url.service';
 
 @Module({
   controllers: [AppController],
+  providers: [TargetUrlService],
+  exports: [TargetUrlService],
 })
 export class CommonModule {}
