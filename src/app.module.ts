8e1d6edb836bdad7982acfa73667ec234c9a11d9
Horace-web <horaceodounlami2006@gmail.com>
2026-09-13 02:26:53 +0100

    Add root application module with all feature modules

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ServicesModule } from './services/services.module';
+import { MonitorsModule } from './monitors/monitors.module';
+import { MonitoringModule } from './monitoring/monitoring.module';
+import { CheckResultsModule } from './check-results/check-results.module';
+import { IncidentsModule } from './incidents/incidents.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { CommonModule } from './common/common.module';
+
+@Module({
+  imports: [
+    PrismaModule,
+    CommonModule,
+    AuthModule,
+    UsersModule,
+    ServicesModule,
+    MonitorsModule,
+    MonitoringModule,
+    CheckResultsModule,
+    IncidentsModule,
+  ],
+})
+export class AppModule {}
