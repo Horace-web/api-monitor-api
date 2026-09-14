ff7dc0bb0df885d9b873b0172442015952ac8605
Horace-web <horaceodounlami2006@gmail.com>
2026-09-14 02:33:10 +0100

    fix: import auth module in check results

--- src/check-results/check-results.module.ts
+++ src/check-results/check-results.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '@/prisma/prisma.module';
+import { AuthModule } from '@/auth/auth.module';
 import { CheckResultsController } from './check-results.controller';
 import { CheckResultsService } from './check-results.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [CheckResultsController],
   providers: [CheckResultsService],
   exports: [CheckResultsService],
