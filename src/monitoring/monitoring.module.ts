40ffedb53ae1dce9d68401d85f548ffefddf327a
Horace-web <horaceodounlami2006@gmail.com>
2026-09-15 06:50:36 +0100

    feat: connect monitoring to incidents

--- src/monitoring/monitoring.module.ts
+++ src/monitoring/monitoring.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from '@/common/common.module';
 import { PrismaModule } from '@/prisma/prisma.module';
+import { IncidentsModule } from '@/incidents/incidents.module';
 import { MonitoringService } from './monitoring.service';
 
 @Module({
-  imports: [PrismaModule, CommonModule],
+  imports: [PrismaModule, CommonModule, IncidentsModule],
   providers: [MonitoringService],
   exports: [MonitoringService],
 })
