511dfce8dac89fa2b6bb80e7ef9fa47d4a011675
Horace-web <horaceodounlami2006@gmail.com>
2026-09-15 06:50:28 +0100

    feat: expose incident handling to monitoring

--- src/incidents/incidents.module.ts
+++ src/incidents/incidents.module.ts
@@ -3,10 +3,12 @@ import { PrismaModule } from '@/prisma/prisma.module';
 import { AuthModule } from '@/auth/auth.module';
 import { IncidentsController } from './incidents.controller';
 import { IncidentsService } from './incidents.service';
+import { NotificationService } from './notification.service';
 
 @Module({
   imports: [PrismaModule, AuthModule],
   controllers: [IncidentsController],
-  providers: [IncidentsService],
+  providers: [IncidentsService, NotificationService],
+  exports: [IncidentsService],
 })
 export class IncidentsModule {}
